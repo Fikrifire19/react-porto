01dec62eb0920abf1fa576af76600a34c694372d
fikri <muhamadfirki64@yahoo.co.id>
2022-03-18 14:37:23 +0700

    About component

---
+++ src/components/about/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='a'>
+        <div className="a-left">
+            <div className="a-card bg"></div>
+            <div className="a-card">
+                <img src="" alt="" className="a-img" />
+            </div>
+        </div>
+        <div className="a-right">
+            <h1 className="a-title">About Me</h1>
+            <p className="a-sub">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit, rerum iure ullam pariatur eaque doloremque asperiores beatae aliquid alias, voluptatum explicabo optio ea, excepturi maxime expedita quaerat tenetur autem fugiat.</p>
+            <p className="desc">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quos recusandae reprehenderit provident enim fuga vitae laborum, officiis doloremque quod ea explicabo minus? Iste ex amet perferendis eligendi veniam quaerat!
+            </p>
+            <div className="a-award">
+                <img src="" alt="" className="a-award-img" />
+                <div className="a-award-texts">
+                    <h4 className="a-award-title">International Design</h4>
+                    <p className="a-award-desc">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error quis aut, a suscipit magni fugiat dolorum delectus illum et perferendis iure eveniet porro libero aliquid rem debitis laborum esse ratione.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
